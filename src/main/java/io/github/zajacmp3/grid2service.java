e98bf0512f90335634ac7d8869bc18300d01eab1
Mateusz Zajac <deejay.zajac+bitbucket@gmail.com>
2016-05-27 15:58:12 +0200

    Added tests and hello world for testing purposes of auto deply

--- src/main/java/io/github/zajacmp3/grid2service.java
+++ src/main/java/io/github/zajacmp3/grid2service.java
@@ -2,6 +2,6 @@ package io.github.zajacmp3;
 
 public class grid2service {
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
     }
 }
---
+++ src/test/java/io/github/zajacmp3/grid2serviceTest.java
@@ -0,0 +1,14 @@
+package io.github.zajacmp3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class grid2serviceTest {
+    @Test
+    public void main() throws Exception {
+        Assert.assertTrue(true);
+    }
+
+}
